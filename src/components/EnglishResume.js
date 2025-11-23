e1652e00718349fe928bd48045f70d85ef8c6b47
Gizem Ece Avsar <geceavsar@gmail.com>
2025-11-23 20:46:24 +0300

    Add mailto: and hide email from robots

--- src/components/EnglishResume.js
+++ src/components/EnglishResume.js
@@ -4,17 +4,28 @@ import meImage from "../assets/images/me.png";
 
 
 const EnglishResume = () => {
+  const [mailto, setMailto] = React.useState("");
+
+  React.useEffect(() => {
+    const user = "geceavsar";
+    const atSign = String.fromCharCode(64); // "@"
+    const domain = ["gm", "ail"].join("");
+    const tld = String.fromCharCode(99, 111, 109); // "com"
+    const email = `${user}${atSign}${domain}.${tld}`;
+    setMailto(`mailto:${email}`);
+  }, []);
+
   return (
     <div className="min-h-screen flex flex-col items-center px-4 bg-transparent fade-in-2s">
 
-    <Header language="en" />
+    <Header barType="en" />
 
       <main className="mt-8 w-full max-w-4xl p-6">
       <section className="mb-8">
         <img src={meImage} alt="Gizem in 2023" className="rounded-lg w-48 mx-auto mb-4" />
           <ul className="list-disc pl-6 space-y-2 text-gray-700 dark:text-white dark:opacity-70">
-          software developer for a while, computer engineer often. sometimes geek, mostly sassy. <br />
-          used to embed software, now a perpetual learner.
+          software developer for a while, computer engineer often, sometimes geek. <br />
+          usually embeds code somewhere.
           </ul>
       </section>
 
@@ -22,7 +33,10 @@ const EnglishResume = () => {
           <h2 className="text-xl font-medium text-gray-800 dark:text-white dark:opacity-70 border-b pb-2 mb-4">Work experience</h2>
           <ul className="list-disc pl-6 space-y-2 text-gray-700 dark:text-white dark:opacity-70">
             <li>
-              <strong>freelancer</strong> (now)
+              <strong>Software Engineer</strong> – Asis CT (now)
+              <p className="text-sm text-gray-600 dark:text-white dark:opacity-70">
+                GNU/Linux, C++, Qt/QML, BitBake, gRPC, CMake, Bash, Docker, Jira
+              </p>
             </li>
             <li>
               <strong>Software Engineer</strong> – Siemens (Aug 2020 - Oct 2024)
@@ -43,10 +57,17 @@ const EnglishResume = () => {
           <h2 className="text-xl font-medium text-gray-800 border-b pb-2 mb-4 dark:text-white dark:opacity-70">Education</h2>
           <ul className="list-disc pl-6 space-y-2 text-gray-700 dark:text-white dark:opacity-70">
             <li>
-              <strong>Computer Engineering</strong> – Istanbul Technical University, 2021
+              <strong>Computer Engineering</strong> – Istanbul Technical University
+              <ul className="list-circle pl-6 space-y-2 text-gray-700 dark:text-white dark:opacity-70">
+                <li>MSc - ongoing</li>
+                <li>BSc - 2021</li>
+              </ul>
             </li>
             <li>
-              <strong>Control and Automation Engineering</strong> – Istanbul Technical University, 2019
+              <strong>Control and Automation Engineering</strong> – Istanbul Technical University
+              <ul className="list-circle pl-6 space-y-2 text-gray-700 dark:text-white dark:opacity-70">
+                <li>BSc - 2019</li>
+              </ul>
             </li>
           </ul>
         </section>
@@ -54,7 +75,9 @@ const EnglishResume = () => {
         <section className="mb-8">
           <h2 className="text-xl font-medium text-gray-800 border-b pb-2 mb-4 dark:text-white dark:opacity-70">Contact</h2>
           <ul className="list-disc pl-6 space-y-2 text-gray-700 dark:text-white dark:opacity-70">
-            geceavsar [at] gmail [dot] com
+              {mailto ? (
+                <a className="underline" href={mailto}>{mailto.replace("mailto:", "")}</a>) : 
+                ("geceavsar [at] gmail [dot] com")}
           </ul>
         </section>         
       </main>
--- src/components/TurkceOzgecmis.js
+++ src/components/TurkceOzgecmis.js
@@ -4,17 +4,28 @@ import meImage from "../assets/images/me.png";
 
 
 const TurkceOzgecmis = () => {
+    const [mailto, setMailto] = React.useState("");
+  
+    React.useEffect(() => {
+      const user = "geceavsar";
+      const atSign = String.fromCharCode(64); // "@"
+      const domain = ["gm", "ail"].join("");
+      const tld = String.fromCharCode(99, 111, 109); // "com"
+      const email = `${user}${atSign}${domain}.${tld}`;
+      setMailto(`mailto:${email}`);
+    }, []);
+
   return (
     <div className="min-h-screen flex flex-col items-center px-4 bg-transparent fade-in-2s">
 
-    <Header language="tr" />
+    <Header barType="tr" />
 
     <main className="mt-8 w-full max-w-4xl p-6">
       <section className="mb-8">
         <img src={meImage} alt="Gizem in 2023" className="rounded-lg w-48 mx-auto mb-4" />
           <ul className="list-disc pl-6 space-y-2 text-gray-700 dark:text-white dark:opacity-70">
-          bir süredir yazılım geliştirici, genelde bilgisayar mühendisi. zaman zaman geek, çoğunlukla süslü. <br />
-          eskiden bir yerlere yazılım gömerdi, şimdi daimi öğrenci.
+          bir süredir yazılım geliştirici, genelde bilgisayar mühendisi, zaman zaman geek. <br />
+          genellikle bir yerlere yazılım gömer.
           </ul>
       </section>
 
@@ -22,7 +33,10 @@ const TurkceOzgecmis = () => {
           <h2 className="text-xl font-medium text-gray-800 dark:text-white dark:opacity-70 border-b pb-2 mb-4">Kariyer</h2>
           <ul className="list-disc pl-6 space-y-2 text-gray-700 dark:text-white dark:opacity-70">
             <li>
-              <strong>freelancer</strong> (şu an)
+              <strong>Software Engineer</strong> – Asis Elektronik (şu an)
+              <p className="text-sm text-gray-600 dark:text-white dark:opacity-70">
+                GNU/Linux, C++, Qt/QML, BitBake, gRPC, CMake, Bash, Docker, Jira
+              </p>
             </li>
             <li>
               <strong>Software Engineer</strong> – Siemens (Ağu 2020 - Eki 2024)
@@ -43,10 +57,17 @@ const TurkceOzgecmis = () => {
           <h2 className="text-xl font-medium text-gray-800 dark:text-white dark:opacity-70 border-b pb-2 mb-4">Eğitim</h2>
           <ul className="list-disc pl-6 space-y-2 text-gray-700 dark:text-white dark:opacity-70">
             <li>
-              <strong>Bilgisayar Mühendisliği</strong> – İstanbul Teknik Üniversitesi, 2021
+              <strong>Bilgisayar Mühendisliği</strong> – İstanbul Teknik Üniversitesi
+              <ul className="list-circle pl-6 space-y-2 text-gray-700 dark:text-white dark:opacity-70">
+                <li>Yüksek lisans - devam ediyor</li>
+                <li>Lisans - 2021</li>
+              </ul>
             </li>
             <li>
-              <strong>Kontrol ve Otomasyon Mühendisliği</strong> – İstanbul Teknik Üniversitesi, 2019
+              <strong>Kontrol ve Otomasyon Mühendisliği</strong> – İstanbul Teknik Üniversitesi
+              <ul className="list-circle pl-6 space-y-2 text-gray-700 dark:text-white dark:opacity-70">
+                <li>Lisans - 2019</li>
+              </ul>
             </li>
           </ul>
         </section>
@@ -54,7 +75,8 @@ const TurkceOzgecmis = () => {
         <section className="mb-8">
           <h2 className="text-xl font-medium text-gray-800 border-b pb-2 mb-4 dark:text-white dark:opacity-70">İletişim</h2>
           <ul className="list-disc pl-6 space-y-2 text-gray-700 dark:text-white dark:opacity-70">
-            geceavsar [at] gmail [dot] com
+              {mailto ? (<a href={mailto}> {mailto.replace("mailto:", "")} </a>) : 
+              ("geceavsar [güzel a] gmail [nokta] com")}
           </ul>
         </section>
       </main>
